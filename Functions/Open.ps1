2c69f080e5466058344e6ec0d64e001fa5b9bb1b
Alex Guerrieri <klaidliadon@gmail.com>
2018-05-19 10:00:31 -0700

    Open: improve folder recognition

--- Functions/Open.ps1
+++ Functions/Open.ps1
@@ -2,7 +2,7 @@ function Open {
     if ($args.Count -eq 0 ) { 
         $args = @(".")
     }
-    if ((Get-Item $args[0]).Attributes -eq "Directory") {
+    if ((Get-Item $args[0]) -is [System.IO.DirectoryInfo]) {
         explorer $args 
     } else {
         & $args[0]
--- Functions/Subl.ps1
+++
@@ -1,6 +0,0 @@
-function Subl { 
-    if ($args.Count -eq 0 ) { 
-        $args = @(".") 
-    }
-    & "C:\Program Files\Sublime Text 3\sublime_text.exe" $args 
-}
